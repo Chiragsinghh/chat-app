0f6071ffd89435e3b90ef0be76707880f3534a36
Chiragsinghh <chiragcse05@gmail.com>
2026-01-01 04:18:01 +0530

    Update message.routes.js

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,12 +1,22 @@
-import express from"express"
+import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar,getMessages,sendMessage } from "../controllers/message.controller.js";
+import { 
+  getMessages, 
+  getUsersForSidebar, 
+  sendMessage, 
+  createGroup,
+  updateGroup,
+  manageGroupMembers // Import the controller we wrote earlier
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
-router.get("/users",protectRoute,getUsersForSidebar)
-router.get("/:id",protectRoute,getMessages)
-router.post("/send/:id",protectRoute,sendMessage)
-
+// New route for group creation
+router.post("/groups", protectRoute, createGroup);
+router.put("/groups/:groupId", protectRoute, updateGroup);
+router.post("/groups/:groupId/members", protectRoute, manageGroupMembers);
 export default router;
