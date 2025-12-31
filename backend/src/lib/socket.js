9b85a8d5d03bae759bdc36066b58ec455d974cd9
Chiragsinghh <chiragcse05@gmail.com>
2026-01-01 04:16:56 +0530

    Update socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,34 +1,46 @@
-import {Server} from "socket.io"
+import { Server } from "socket.io";
 import express from "express";
 import http from "http";
 
 export const app = express();
 export const server = http.createServer(app);
 
-export const io = new Server(server,{
-    cors:{
-        origin:["http://localhost:5173"]
-    }
+export const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
 });
 
-export function getRecieverSocketId(userId){
-    return userSocketMap[userId];
+export function getRecieverSocketId(userId) {
+  return userSocketMap[userId];
 }
 
-const userSocketMap={};//
+const userSocketMap = {}; 
 
-io.on("connection", (socket) =>{
-    console.log("A user connected",socket.id);
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+    console.log(`User ${userId} connected with socket ${socket.id}`);
+  }
 
-    const userId = socket.handshake.query.userId;
-    if(userId) userSocketMap[userId] = socket.id
+  // Join a unique room for every group the user belongs to
+  // Note: Usually you'd fetch user groups from DB here, 
+  // but for simplicity, we'll let the frontend trigger 'joinGroup'
+  socket.on("joinGroup", (groupId) => {
+    socket.join(groupId);
+    console.log(`User ${userId} joined group room: ${groupId}`);
+  });
 
-    io.emit("getOnlineUsers",Object.keys(userSocketMap))
+  socket.on("leaveGroup", (groupId) => {
+    socket.leave(groupId);
+  });
 
-    socket.on("dissconnect", ()=>{
-        console.log("A user dissconnected", socket.id)
-        delete userSocketMap[userId]
-        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    })
-})
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
